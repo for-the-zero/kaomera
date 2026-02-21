9b837598c49aec91f3a56e875fbebfbe96b55cd5
for-the-zero <forthezero@outlook.com>
2026-02-21 15:36:26 +0800

    del displayer.vue & 2 btn in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
     import DrawerBtn from './parts/drawer.vue';
-    import Displayer from './parts/displayer.vue';
+    import { Button } from '@/components/ui/button';
+    import { Play, Pause } from 'lucide-vue-next';
+    import { useGlbStore } from '@/utils/glb';
+
+    const { outputs, status } = useGlbStore();
+    function displayerCopy(){
+        navigator.clipboard.writeText(outputs[0]);
+    };
 </script>
 
 <template>
     <DrawerBtn />
-    <Displayer />
+    <Button variant="secondary"
+        class="fixed bottom-7.5 left-1/2 transform -translate-x-1/2 text-3xl w-auto h-auto px-6 py-4"
+        v-if="outputs.length >= 1 && status.isRunning"
+        @click="displayerCopy"
+    >
+        {{ outputs[0] }}
+    </Button>
+    <Button :variant="status.isRunning ? 'destructive' : 'default'"
+        class="fixed bottom-7.5 left-7.5"
+        @click="()=>{status.isRunning = !status.isRunning}"
+    >
+        <Pause v-if="status.isRunning" />
+        <Play v-else />
+        {{ status.isRunning ? "停止" : "启动" }}
+    </Button>
 </template>
